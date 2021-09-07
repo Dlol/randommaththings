378350d77022ec899caf8622a8debad888eb1c0d
Dlol <dravennadas@gmail.com>
2021-09-07 18:11:10 +0000

    Added distance function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 #include "vectors.hpp"
 #include "geo.hpp"
 
-using namespace std;
-
 int main(){
-  cout << fibbonacci(13) << endl;
-  vector2 bector (3,3);
-  cout << bector << endl;
+  std::cout << "just for testing" << std::endl;
 }
